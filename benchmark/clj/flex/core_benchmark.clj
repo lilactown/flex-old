70c4b17941c5fc343ec7262a479aa24e60362f9f
Will Acton <will@willacton.com>
2021-03-23 18:53:35 -0700

    add profile alias

--- benchmark/clj/flex/core_benchmark.clj
+++ benchmark/clj/flex/core_benchmark.clj
@@ -4,53 +4,72 @@
    [criterium.core :as c]))
 
 
+#_(require '[clj-async-profiler.core :as prof])
+
+
 (defn fib*
-  [limit]
-  (loop [prev 0
-         cur 1]
-    (if (< cur limit)
-      (recur cur (+ prev cur))
-      prev)))
+  ([limit] (fib* limit [0 1]))
+  ([limit [prev cur]]
+   (let [next (+ prev cur)]
+     (if (< next limit)
+       (recur limit [cur next])
+       [prev cur]))))
+
+(defn run-graph! [times]
+  (let [env (f/env)
+        db (f/input {:limit 0 :chars "a"})
+        limit (f/signal (:limit @db))
+        chars (f/signal (:chars @db))
+        fib (f/signal ([] (fib* @limit))
+                      ([v] (fib* @limit v)))
+        combo (f/signal [(second @fib) @chars])
+        ;; combinations (f/collect [] combo)
+        ]
+    (f/with-env env
+      (f/connect! combo))
+    (doseq [_ (range times)]
+      (f/with-env env
+        @(f/send db update :limit inc)
+        @(f/send db update :chars str "b")))
+    (f/with-env env @combo)))
+
+#_(do (time (run-graph! 100000)) nil)
+
+#_(c/quick-bench (run-graph! 1000))
+
+#_(prof/profile (run-graph! 1000))
+
+#_(prof/serve-files 8080)
+
+(defn run-calc! [times]
+  (loop [n times
+         limit? true
+         db {:limit 0 :chars "a"}
+         ;; combinations []
+         fib (fib* 0)]
+    (if (zero? n)
+      #_combinations fib
+      (let [limit (:limit db)
+            chars (:chars db)
+            ;; recalculate all fibs every time
+            fib (fib* limit)]
+        (if limit?
+          (recur
+           n
+           (not limit?)
+           (-> db
+               (update :limit inc))
+           #_(conj combinations (vector (second fib) chars))
+           fib)
+          (recur
+           (dec n)
+           (not limit?)
+           (-> db
+               (update :chars str "b"))
+           #_(conj combinations (vector (second fib) chars))
+           fib))))))
 
 
+#_(c/quick-bench (run-times! 1000))
 
-(c/quick-bench
- (let [env (f/env)
-       db (f/input {:limit 0 :chars "a"})
-       limit (f/signal #(:limit @db))
-       chars (f/signal #(:chars @db))
-       fib (f/signal #(fib* @limit))
-       combo (f/signal #(vector @fib @chars))
-       combinations (doto (f/collect [] combo)
-                      (as-> c (f/with-env env
-                                (f/connect! c))))]
-   (doseq [_ (range 10)]
-     (f/with-env env
-       @(f/send db update :limit inc)
-       @(f/send db update :chars str "b")))))
-;; => [[0 "a"] [0 "ab"] [1 "ab"] [1 "abb"] [2 "abb"] [2 "abbb"] [3 "abbb"] [3 "abbbb"] [3 "abbbbb"] [5 "abbbbb"] [5 "abbbbbb"] [5 "abbbbbbb"] [5 "abbbbbbbb"] [8 "abbbbbbbb"] [8 "abbbbbbbbb"] [8 "abbbbbbbbbb"]]
-
-(c/quick-bench
- (loop [n 10
-        limit? true
-        db {:limit 0 :chars "a"}
-        combinations []]
-   (if (zero? n)
-     combinations
-     (let [limit (:limit db)
-           chars (:chars db)
-           fib (fib* limit)]
-       (if limit?
-         (recur
-          n
-          (not limit?)
-          (-> db
-              (update :limit inc))
-          (conj combinations (vector fib chars)))
-         (recur
-          (dec n)
-          (not limit?)
-          (-> db
-              (update :chars str "b"))
-          (conj combinations (vector fib chars))))))))
-;; => [[0 "a"] [0 "a"] [0 "ab"] [1 "ab"] [1 "abb"] [2 "abb"] [2 "abbb"] [3 "abbb"] [3 "abbbb"] [3 "abbbb"] [3 "abbbbb"] [5 "abbbbb"] [5 "abbbbbb"] [5 "abbbbbb"] [5 "abbbbbbb"] [5 "abbbbbbb"] [5 "abbbbbbbb"] [8 "abbbbbbbb"] [8 "abbbbbbbbb"] [8 "abbbbbbbbb"]]
+#_(do (time (run-calc! 100000)) nil)
